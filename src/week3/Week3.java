72d71fcaddf9ee30c885ce4b572afa92b4bc8cb6
suphit <souphitpmc@gmail.com>
2018-10-02 23:04:01 +0700

    Update

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,15 +1,27 @@
 package week3;
 
+import java.util.Scanner;
 public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if(m>=n)
+            return m;
+        else
+            return n;
+
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int i, min=array[0];
+        for(i=1;i<array.length;i++){
+            if(array[i]<min)
+            {
+                min =array[i];
+            }
+        }
+        return  min;
     }
 
     /**
@@ -18,8 +30,32 @@ public class Week3 {
      * @param height chiều cao
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
-    public static String calculateBMI(double weight, double height) {
+    private static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI = weight/(height*height);
+        String Display="";
+        if ( BMI<18.5){
+            Display="Thiếu cân";
+        }
+        if ( 18.5<=BMI && BMI<=22.99){
+            Display ="Bình thường";
+        }
+        if ( BMI>=23&&BMI<=24.99){
+            Display ="Thừa cân";
+        }
+        if ( BMI>=25){
+            Display ="Béo phì";
+        }
+        return Display;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Double nang,cao;
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Please enter your weight >>> ");
+        nang= scan.nextDouble();
+        System.out.print("Please enter your height >>> ");
+        cao= scan.nextDouble();
+        System.out.print(calculateBMI(nang,cao));
     }
 }
