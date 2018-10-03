f0b189563e5ac41ad3bd3b6ad4ab907d0f99dad3
suphit <souphitpmc@gmail.com>
2018-10-03 10:06:08 +0700

    Update

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,78 +1,67 @@
 package week1;
 
 public class Student {
+
     // TODO: khai báo các thuộc tính cho Student
     private String name;
     private String id;
     private String group;
     private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String stdname){
-        this.name= stdname;
+    public void setName(String name) {
+        this.name = name;
     }
-    public String getID(){
+    public String getId() {
         return id;
     }
-    public void setID(String stdid){
-        this.id = stdid;
+    public void setId(String id) {
+        this.id = id;
     }
-    public String getGroup(){
+    public String getGroup() {
         return group;
     }
-    public void setGroup(String groupname){
-
-        this.group = groupname;
+    public void setGroup(String group) {
+        this.group = group;
     }
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
-    public void setEmail(String stdmail) {
-
-        this.email = stdmail;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    /**
-     * Constructor 1
-     */
     Student() {
         // TODO:
         this.name = "Student";
         this.id = "000";
-        this.group = "INT22041";
         this.email = "uet@vnu.edu.vn";
+        this.group = "INT 22041";
     }
 
-    /**
-     * Constructor 2
-     * @param n : ten SV
-     * @param sid : ID
-     * @param em : ten Email
-     */
     Student(String n, String sid, String em) {
         // TODO:
         this.name = n;
         this.id = sid;
-        this.group = "INT22041";
         this.email = em;
+        this.group = "INT 22042";
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
     Student(Student s) {
         // TODO:
-        this.name = s.getName();
-        this.id = s.getID();
-        this.group = s.getGroup();
-        this.email = s.getEmail();
+        this.name = s.name;
+        this.id = s.id;
+        this.email = s.email;
+        this.group = s.group;
     }
 
     String getInfo() {
         // TODO:
-        return this.getName()+"\n"+this.getID()+"\n"+this.getGroup()+"\n"+this.getEmail();
+        return this.getName() + "\n"
+                + this.getId() + "\n"
+                + this.getGroup() + "\n"
+                + this.getEmail();
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,97 +1,87 @@
+package week1;
+
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-    public Student[] students = new Student[100];
+    Student students[] = new Student[100];
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public  boolean sameGroup(Student s1, Student s2) {
         // TODO:
         return s1.getGroup().equals(s2.getGroup());
     }
 
     void studentsByGroup() {
         // TODO:
-        int k = this.students.length;
-        System.out.println("Danh sach sinh vien lop INT22041:");
-        for (int i = 0; i < this.students.length; i++) {
-            if (this.students[i].getGroup().equals("INT22041") && this.students[i].getName()!= "Student") {
+        System.out.println("List of student in classroom INT 22041");
+        for(int i=0;students[i]!=null;i++)
+        {
+            if(this.students[i].getGroup().equals("INT 22041"))
+            {
                 System.out.println(this.students[i].getInfo());
             }
         }
-        System.out.println("Danh sach sinh vien lop INT22044:");
-        for (int i = 0; i < this.students.length; i++) {
-            if (this.students[i].getGroup().equals("INT22044") && this.students[i].getName()!= "Student") {
+        System.out.println("List of student in classroom INT 22042");
+        for(int i=0;students[i]!=null;i++)
+        {
+            if(this.students[i].getGroup().equals("INT 22042"))
+            {
                 System.out.println(this.students[i].getInfo());
             }
         }
+
     }
 
     void removeStudent(String id) {
         // TODO:
-        int i,j;
-        for (i = 0; i < this.students.length; i++) {
-            if (this.students[i].getID().equals(id)) {
+        int vitri = 0 , n = students.length;
+        for(int i=0;i<n;i++)
+        {
+            if(this.students[i].getId().equals(id))
+            {
+                vitri = i;
                 break;
             }
         }
-        for (j = i; j < (this.students.length)-1; j++) {
-            this.students[j] = this.students[j + 1];
+        for(int i = vitri ; i<n-1;i++)
+        {
+            this.students[i] = this.students[i+1];
         }
-        this.students[(this.students.length)-1] = new Student();
+        this.students[n-1] = null;
+        n--;
     }
-
-
     public static void main(String[] args) {
         // TODO:
-   
-        Student s = new Student();
-        
-        
-        
-        Student s1 = new Student();
-        s1.setName("Su phit Phom machan");
-        s1.setID("17020190");
-        s1.setGroup("INT22044");
-        s1.setEmail("souphitpmc@gmail.com");
-        System.out.println(s1.getInfo());
-        
-        
-        
-        
-        Student s2 = new Student();
-        Student s3 = new Student("Kham may","15020190","khammay@gmail.com");
-        Student s4 = new Student(s3);
-        System.out.println(s2.getInfo());
-        System.out.println(s3.getInfo());
-        System.out.println(s4.getInfo());
-        
-        
-        
-        StudentManagement stdm = new StudentManagement();
-        Student s5 = new Student("Som phong","14020190","somphong@gmail.com");
-        System.out.println(stdm.sameGroup(s1,s3));
-        System.out.println(stdm.sameGroup(s3,s5));
-        
-        
-        
-        Student[] stlist = stdm.students;
-        for (int i=0;i<100;i++){
-            stlist[i] = new Student();
-        }
-       
-        
-        
-        
-        stlist[0]=s1;
-        stlist[1]=s3;
-        stlist[2]=s5;
-        stdm.studentsByGroup();
-        System.out.println("Danh sach sinh vien sau khi xoa ID: 15020190");
-        stdm.removeStudent("15020190");
-        for (int i=0;i<stdm.students.length;i++){
-            if (stdm.students[i].getName()!="Student"){
-                System.out.println(stdm.students[i].getInfo());
-            }
-        }
+        Student hocSinh1 = new Student();
+
+        Student hocSinh2 = new Student("arm" , "17020190" , "arm@vnu.edu.vn");
+
+        Student hocSinh3 = new Student(hocSinh2);
+
+        StudentManagement st = new StudentManagement();
+
+        st.students[0]= new Student();
+        st.students[0].setName("tink");
+        st.students[0].setId("19020191");
+        st.students[0].setGroup("INT 22041");
+        st.students[0].setEmail("tink@vnu.edu.vn");
+
+        st.students[1] = new Student();
+        st.students[1].setName("pda");
+        st.students[1].setId("19020190");
+        st.students[1].setGroup("INT 22041");
+        st.students[1].setEmail("19020195@vnu.edu.vn");
+
+        st.students[2] = new Student();
+        st.students[2].setName("kham");
+        st.students[2].setId("19020190");
+        st.students[2].setGroup("INT 22042");
+        st.students[2].setEmail("19020190@vnu.edu.vn");
+
+        System.out.println(st.sameGroup(hocSinh1,hocSinh2) + "\n"+ st.sameGroup(hocSinh2,hocSinh3));
+        st.removeStudent("19020190");
+        System.out.println("List of students : " + "\n");
+        st.studentsByGroup();
 
     }
+
 }
