42ac0d17a4afbef6b741090ae27bfa2797c457cf
suphit <souphitpmc@gmail.com>
2018-10-10 09:51:28 +0700

    Update

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -7,3 +7,85 @@
  * @version 0.1
  */
 package week4.task1;
+//lớp cha
+class HoaQua{
+    protected int price;
+    protected String from;
+    protected String D_import;
+    protected int amount;
+}
+
+//tạo Các lớp con
+class QuaCam extends HoaQua{
+
+}
+class QuaTao extends HoaQua{
+
+}
+class CamCaoPhong extends HoaQua{
+
+}
+class CamSanh extends HoaQua{
+
+}
+class main{
+    public static void main(String[] args) {
+        // đặt các đối tượng cho các lớp con
+        QuaCam fruit1=new QuaCam();
+        fruit1.price=50000;
+        fruit1.from="Hanoi";
+        fruit1.D_import="23/09/2018";
+        fruit1.amount=50;
+
+        QuaTao fruit2=new QuaTao();
+        fruit2.price=65000;
+        fruit2.from="China";
+        fruit2.D_import="02/10/2018";
+        fruit2.amount=80;
+
+
+        CamCaoPhong fruit3=new CamCaoPhong();
+        fruit3.price=45000;
+        fruit3.from="HoaBinh";
+        fruit3.D_import="01/08/2018";
+        fruit3.amount=74;
+
+
+        CamSanh fruit4=new CamSanh();
+        fruit4.price=55000;
+        fruit4.from="Vien Chan";
+        fruit4.D_import="26/08/2018";
+        fruit4.amount=65;
+
+
+        //in ra màn hình
+        System.out.println("=======================");
+        System.out.println("Qua Cam : ");
+        System.out.println("Price = "+fruit1.price+" VND/kg");
+        System.out.println("Import from = "+fruit1.from);
+        System.out.println("Date of Import = "+fruit1.D_import);
+        System.out.println("Amount import = "+fruit1.amount+" Kg");
+
+        System.out.println("=======================");
+        System.out.println("Qua Tao : ");
+        System.out.println("Price = "+fruit2.price+" VND/Kg");
+        System.out.println("Import from = "+fruit2.from);
+        System.out.println("Date of Import = "+fruit2.D_import);
+        System.out.println("Amount import = "+fruit2.amount+" Kg");
+
+        System.out.println("=======================");
+        System.out.println("Cam Cao Phong : ");
+        System.out.println("Price = "+fruit3.price+" VND/Kg");
+        System.out.println("Import from = "+fruit3.from);
+        System.out.println("Date of Import = "+fruit3.D_import);
+        System.out.println("Amount import = "+fruit3.amount+" Kg");
+
+        System.out.println("=======================");
+        System.out.println("Cam Sanh : ");
+        System.out.println("Price = "+fruit4.price+" VND/Kg");
+        System.out.println("Import from = "+fruit4.from);
+        System.out.println("Date of Import = "+fruit4.D_import);
+        System.out.println("Amount import = "+fruit4.amount+" Kg");
+
+        }
+}
--- src/week4/task2/package-info.java
+++ src/week4/task2/package-info.java
@@ -7,3 +7,137 @@
  * @version 0.1
  */
 package week4.task2;
+class Shape{
+    String color="red";
+    boolean filled=true;
+    public Shape(){
+    }
+    public Shape(String color, Boolean filled) {
+        this.filled = filled;
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+    public Boolean isFilled() {
+        return filled;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+    public String toString() {
+        return "Shape's information : {color = " + color + ", filled = " + filled + "}";
+    }
+}
+class Circle extends Shape{
+    private double radius=1.0d;
+    private final double Pi=3.14d;
+    public Circle(){
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(String color, Boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Pi*radius*radius;
+    }
+    public double Perimeter(){
+        return 2*Pi*radius;
+    }
+    public String toString() {
+
+        return "Circle's information : {" + "radius=" + radius + ", color=" + color + ", filled = " + filled + "}";
+    }
+}
+class Rectangle extends Shape{
+    double Width = 1.0d;
+    private double Length = 1.0d;
+    public Rectangle() {
+    }
+    public Rectangle(double width, double length) {
+        Width = width;
+        Length = length;
+    }
+    public Rectangle(String color, Boolean filled, double width, double length) {
+        super(color, filled);
+        Width = width;
+        Length = length;
+    }
+    public double getWidth() {
+        return Width;
+    }
+    public double getLength() {
+        return Length;
+    }
+    public void setWidth(double width) {
+        Width = width;
+    }
+    public void setLength(double length) {
+        Length = length;
+    }
+    public double setArea(){
+        return Width*Length;
+    }
+    public double Perimeter(){
+        return (Width+Length)*2;
+    }
+    public String toString() {
+        return "Rectangle's information : {" + "color = " + color + ", filled = " + filled + ", Width = " + Width + ", Length=" + Length + "}";
+    }
+}
+class Square extends Rectangle{
+    public Square() {
+    }
+    public Square(double side) {
+        super(side,side);
+    }
+    public Square(double side, String color, boolean filled){
+        super(color,filled,side,side);
+
+    }
+    public double getSide(){
+        return Width;
+    }
+    public void setSide(double side){
+        side = Width;
+    }
+    public void setWidth(double side){
+        side = Width;
+    }
+    public void setLenght(double side){
+        side = Width;
+    }
+    public String toString() {
+        return "Square's information = {" + "color = " + color  + ", filled = " + filled + ", Side = " + Width + "}";
+    }
+}
+class main{
+    public static void main(String[] args) {
+
+        Shape s= new Shape();
+        Circle c= new Circle();
+        Rectangle r = new Rectangle();
+        Square sq = new Square();
+        System.out.println(s.toString());
+        System.out.println(c.toString());
+        System.out.println("Circle's area is :"+c.getArea());
+        System.out.println("Circle's perimeter is :"+c.Perimeter());
+        System.out.println(r.toString());
+        System.out.println("Rectangle's area is :"+r.setArea());
+        System.out.println("Rectangle's Perimeter is :"+r.Perimeter());
+        System.out.println(sq.toString());
+
+    }
+}
